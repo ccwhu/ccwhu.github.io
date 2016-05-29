9315625a03b59f2246e275ca6670c624bce41f90
ccwhu <guokai827755806@126.com>
2016-05-29 17:23:46 +0800

    modify bgm

--- Love/js/functions_dev.js
+++ Love/js/functions_dev.js
@@ -134,3 +134,19 @@ function adjustCodePosition() {
 function showLoveU() {
 	$('#loveu').fadeIn(3000);
 }
+ 
+function audioAutoPlay(id){  
+    var audio = document.getElementById(id),  
+        play = function(){  
+            audio.play();  
+            document.removeEventListener("touchstart",play, false);  
+        };  
+    audio.play();  
+    document.addEventListener("WeixinJSBridgeReady", function () {  
+        play();  
+    }, false);  
+    document.addEventListener('YixinJSBridgeReady', function() {  
+        play();  
+    }, false);  
+    document.addEventListener("touchstart",play, false);  
+}  
